96d870c469b2ad8e61fcfbe62cd4b3fe82de80f1
Isac Sandin Ribeiro <isacsandim@gmail.com>
2014-08-28 22:41:55 -0300

    Adicionado lógica de spash login; Muitas refatorações no código(arquivos inúteis deletados, recursos renomeados )

---
+++ src/com/myselfie/myselfie/ui/SettingsFragment.java
@@ -0,0 +1,52 @@
+package com.myselfie.myselfie.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.widget.LoginButton;
+import com.myselfie.myselfie.R;
+
+public class SettingsFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_setings, container, false);
+		LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+		authButton.setFragment(this);
+		return view;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);		
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
+}
---
+++ src/com/myselfie/myselfie/ui/TabsFragment.java
@@ -0,0 +1,236 @@
+package com.myselfie.myselfie.ui;
+
+import java.util.ArrayList;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TabHost.TabContentFactory;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
+
+import com.myselfie.myselfie.R;
+
+@SuppressLint("InflateParams")
+public class TabsFragment extends Fragment {
+    TabManager mTabManager;
+    TextView statusText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mTabManager = new TabManager(getActivity(), getChildFragmentManager(),
+                R.id.realtabcontent);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tabs, container, false);
+        statusText = (TextView) v.findViewById(R.id.tv_title);
+        mTabManager.handleCreateView(v);        
+        mTabManager.addTab(R.string.lbl_master_detail, R.drawable.ico_star,MasterDetailFragment.class, savedInstanceState);
+        mTabManager.addTab(R.string.lbl_swipe, R.drawable.ico_list, MasterDetailFragment.class,savedInstanceState);
+        mTabManager.addTab(R.string.lbl_check_list, R.drawable.ico_check,MasterDetailFragment.class, savedInstanceState);
+        mTabManager.addTab(R.string.lbl_settings, R.drawable.ico_settings,SettingsFragment.class,savedInstanceState);
+        
+        return v;
+    }
+
+    @Override
+    public void onViewStateRestored(Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        mTabManager.handleViewStateRestored(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mTabManager.handleDestroyView();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mTabManager.handleSaveInstanceState(outState);
+    }
+
+    /**
+     * This is a helper class that implements a generic mechanism for
+     * associating fragments with the tabs in a tab host.  DO NOT USE THIS.
+     * If you want tabs in a fragment, use the support v13 library's
+     * FragmentTabHost class, which takes care of all of this for you (in
+     * a simpler way even).
+     */
+    public class TabManager implements TabHost.OnTabChangeListener {
+        private final Context mContext;
+        private final FragmentManager mManager;
+        private final int mContainerId;
+        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+        private TabHost mTabHost;
+        private TabInfo mLastTab;
+        private boolean mInitialized;
+        private String mCurrentTabTag;
+
+        final class TabInfo {
+            private final String tag;
+            private final Class<?> clss;
+            private final Bundle args;
+            private Fragment fragment;
+
+            TabInfo(String _tag, Class<?> _class, Bundle _args) {
+                tag = _tag;
+                clss = _class;
+                args = _args;
+            }
+        }
+
+        public TabManager(Context context, FragmentManager manager, int containerId) {
+            mContext = context;
+            mManager = manager;
+            mContainerId = containerId;
+        }
+
+        public TabHost handleCreateView(View root) {
+            if (mTabHost != null) {
+                throw new IllegalStateException("TabHost already set");
+            }
+            mTabHost = (TabHost)root.findViewById(android.R.id.tabhost);
+            mTabHost.setup();
+            mTabHost.setOnTabChangedListener(this);
+            return mTabHost;
+        }
+
+        public void addTab(int labelID,int drawableId, Class<?> clss, Bundle args) {
+            
+        	View tabview = createTabView(mTabHost.getContext(),drawableId);
+        	String tag = mTabHost.getContext().getString(labelID);
+        	
+            TabSpec setContent = mTabHost.newTabSpec(tag).setIndicator(tabview).setContent(new TabContentFactory() {
+                    public View createTabContent(String tag) {
+                    	View view = new View(mContext);
+                    	view.setMinimumWidth(0);
+                    	view.setMinimumHeight(0);
+                    	return view;}
+            });
+	        TabInfo info = new TabInfo(tag, clss, args);
+	        mTabs.add(info);
+            mTabHost.addTab(setContent);
+            
+        }
+        
+        private View createTabView(final Context context, int drawableId) {
+            View view = LayoutInflater.from(context).inflate(R.layout.tab_indicator, null);
+            ImageView iv = (ImageView) view.findViewById(R.id.icon);
+            iv.setImageDrawable(context.getResources().getDrawable(drawableId));
+            return view;
+        }
+
+        public void handleViewStateRestored(Bundle savedInstanceState) {
+            if (savedInstanceState != null) {
+                mCurrentTabTag = savedInstanceState.getString("tab");
+            }
+            mTabHost.setCurrentTabByTag(mCurrentTabTag);
+
+            String currentTab = mTabHost.getCurrentTabTag();
+
+            // Go through all tabs and make sure their fragments match
+            // the correct state.
+            FragmentTransaction ft = null;
+            for (int i=0; i<mTabs.size(); i++) {
+                TabInfo tab = mTabs.get(i);
+                tab.fragment = mManager.findFragmentByTag(tab.tag);
+                if (tab.fragment != null && !tab.fragment.isDetached()) {
+                    if (tab.tag.equals(currentTab)) {
+                        // The fragment for this tab is already there and
+                        // active, and it is what we really want to have
+                        // as the current tab.  Nothing to do.
+                        mLastTab = tab;
+                    } else {
+                        // This fragment was restored in the active state,
+                        // but is not the current tab.  Deactivate it.
+                        if (ft == null) {
+                            ft = mManager.beginTransaction();
+                        }
+                        ft.detach(tab.fragment);
+                    }
+                }
+            }
+
+            // We are now ready to go.  Make sure we are switched to the
+            // correct tab.
+            mInitialized = true;
+            ft = doTabChanged(currentTab, ft);
+            if (ft != null) {
+                ft.commit();
+                mManager.executePendingTransactions();
+            }
+        }
+
+        public void handleDestroyView() {
+            mCurrentTabTag = mTabHost.getCurrentTabTag();
+            mTabHost = null;
+            mTabs.clear();
+            mInitialized = false;
+        }
+
+        public void handleSaveInstanceState(Bundle outState) {
+            outState.putString("tab", mTabHost != null
+                    ? mTabHost.getCurrentTabTag() : mCurrentTabTag);
+        }
+
+        @Override
+        public void onTabChanged(String tabId) {
+            if (!mInitialized) {
+                return;
+            }
+            statusText.setText(tabId);
+            FragmentTransaction ft = doTabChanged(tabId, null);
+            if (ft != null) {
+                ft.commit();
+            }
+        }
+
+        private FragmentTransaction doTabChanged(String tabId, FragmentTransaction ft) {
+            TabInfo newTab = null;
+            for (int i=0; i<mTabs.size(); i++) {
+                TabInfo tab = mTabs.get(i);
+                if (tab.tag.equals(tabId)) {
+                    newTab = tab;
+                }
+            }
+            if (newTab == null) {
+                throw new IllegalStateException("No tab known for tag " + tabId);
+            }
+            if (mLastTab != newTab) {
+                if (ft == null) {
+                    ft = mManager.beginTransaction();
+                }
+                if (mLastTab != null) {
+                    if (mLastTab.fragment != null) {
+                        ft.detach(mLastTab.fragment);
+                    }
+                }
+                if (newTab != null) {
+                    if (newTab.fragment == null) {
+                        newTab.fragment = Fragment.instantiate(mContext,
+                                newTab.clss.getName(), newTab.args);
+                        ft.add(mContainerId, newTab.fragment, newTab.tag);
+                    } else {
+                        ft.attach(newTab.fragment);
+                    }
+                }
+                mLastTab = newTab;
+            }
+            return ft;
+        }
+    }
+}
