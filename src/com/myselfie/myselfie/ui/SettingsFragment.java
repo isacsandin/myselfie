52fa6f9ef6734697d68b435ea74819e3474f800c
Alan Cardoso <xmballd@gmail.com>
2014-09-10 11:34:06 -0300

    logout

--- src/com/myselfie/myselfie/ui/SettingsFragment.java
+++ src/com/myselfie/myselfie/ui/SettingsFragment.java
@@ -3,20 +3,60 @@ package com.myselfie.myselfie.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.myselfie.myselfie.R;
+import com.sromku.simple.fb.SimpleFacebook;
+import com.sromku.simple.fb.listeners.OnLogoutListener;
 
 public class SettingsFragment extends Fragment {
+	private static final String TAG = "SettingsFragment";
+	private SimpleFacebook mSimpleFacebook;
+	private Button mButtonLogout;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_setings, container, false);
-		Button authButton = (Button) view.findViewById(R.id.authButton);
-		//authButton.setFragment(this);
+		View view = inflater.inflate(R.layout.fragment_settings, container, false);
+		
+		mSimpleFacebook = SimpleFacebook.getInstance(this.getActivity());
+		mButtonLogout = (Button) view.findViewById(R.id.logout_button);
+		
+		mButtonLogout.setText("Login via Facebook");		
+		mButtonLogout.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// logout listener
+				OnLogoutListener onLogoutListener = new OnLogoutListener() {
+				    @Override
+				    public void onLogout() {
+				        Log.i(TAG, "You are logged out");
+				    }
+		
+				    @Override
+					public void onThinking() {
+						// TODO Auto-generated method stub
+					}
+		
+					@Override
+					public void onException(Throwable throwable) {
+						// TODO Auto-generated method stub
+					}
+		
+					@Override
+					public void onFail(String reason) {
+						// TODO Auto-generated method stub
+					}
+				};
+				
+				mSimpleFacebook.logout(onLogoutListener);
+			}
+		});
+		
 		return view;
 	}
 
